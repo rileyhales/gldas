1107416dc1bc29adff5cb35a4407a4dcadad9673
rileyhales <rileyhales1@gmail.com>
2020-05-18 15:44:49 -0600

    geomatics v0.8 and conda package update

--- tethysapp/gldas/charts.py
+++ tethysapp/gldas/charts.py
@@ -6,15 +6,15 @@ Description: Functions for generating timeseries and simple statistical
 """
 import calendar
 import datetime as dt
-import os
 import glob
 import json
+import os
 
-import netCDF4 as nc
 import geomatics as gm
+import netCDF4 as nc
 
-from .options import gldas_variables
 from .app import Gldas as App
+from .options import gldas_variables
 
 
 def newchart(data):
@@ -32,19 +32,16 @@ def newchart(data):
             break
 
     user_workspace = os.path.join(os.path.dirname(__file__), 'workspaces', 'user_workspaces', data['instance_id'])
-    date_pattern = 'GLDAS_NOAH025_M.A%Y%m.021.nc4'
+    # date_pattern = 'GLDAS_NOAH025_M.A%Y%m.021.nc4'
 
     # list then filter the available netcdfs
     path = os.path.join(App.get_custom_setting('thredds_path'), 'raw')
-    allfiles = os.listdir(path)
     if len(data['time']) == 5:  # a decade choice
-        filefilter = 'A' + data['time'][0:3]
-        files = [i for i in allfiles if filefilter in i and i.endswith('.nc4')]
+        files = glob.glob(os.path.join(path, f"*A{data['time'][0:3]}*.nc4"))
     elif len(data['time']) == 4:  # a year
-        filefilter = 'A' + data['time'][0:4]
-        files = [i for i in allfiles if filefilter in i and i.endswith('.nc4')]
+        files = glob.glob(os.path.join(path, f"*A{data['time'][0:4]}*.nc4"))
     else:
-        files = [i for i in allfiles if i.endswith('.nc4')]
+        files = glob.glob(os.path.join(path, "*.nc4"))
     files = [os.path.join(path, file) for file in files]
     files.sort()
 
@@ -52,49 +49,47 @@ def newchart(data):
 
     # get the timeseries, units, and message based on location type
     if data['loc_type'] == 'Point':
-        timeseries = gm.netcdfs.point_series(files, data['variable'], data['coords'], date_pattern)
+        timeseries = gm.times.point_series(files, data['variable'], data['coords'], x_var='lon', y_var='lat')
         meta['seriesmsg'] = 'At a Point'
 
     elif data['loc_type'] == 'Polygon':
         coords = data['coords'][0]
         coords = (
-            float(coords[0][0]),
-            float(coords[0][1]),
-            float(coords[2][0]),
-            float(coords[2][1]),
+            (float(coords[0][0]), float(coords[0][1]),),
+            (float(coords[2][0]), float(coords[2][1]),),
         )
-        timeseries = gm.netcdfs.box_series(files, data['variable'], coords, date_pattern)
+        timeseries = gm.times.box_series(files, data['variable'], coords, x_var='lon', y_var='lat')
         meta['seriesmsg'] = 'In a Bounding Box'
 
     elif data['loc_type'] == 'Shapefile':
         shp = [i for i in os.listdir(user_workspace) if i.endswith('.shp')]
-        shp = shp.remove('usergj.shp')
+        shp.remove('usergj.shp')
         shp = os.path.join(shp[0])
-        timeseries = gm.netcdfs.shp_series(files, data['variable'], shp, date_pattern)
+        timeseries = gm.times.shp_series(files, data['variable'], shp, x_var='lon', y_var='lat')
         meta['seriesmsg'] = 'In User\'s Shapefile'
 
     elif data['loc_type'] == 'GeoJSON':
         shp = os.path.join(user_workspace, '__tempgj.shp')
         with open(os.path.join(user_workspace, 'usergj.geojson')) as f:
-            gm.geojsons.geojson_to_shp(json.loads(f.read()), shp)
-        timeseries = gm.netcdfs.shp_series(files, data['variable'], shp, date_pattern)
+            gm.convert.geojson_to_shapefile(json.loads(f.read()), shp)
+        timeseries = gm.times.shp_series(files, data['variable'], shp, x_var='lon', y_var='lat')
         for file in glob.glob(os.path.join(user_workspace, '__tempgj.*')):
             os.remove(file)
         meta['seriesmsg'] = 'In User\'s GeoJSON'
 
     elif data['loc_type'].startswith('esri-'):
         esri_location = data['loc_type'].replace('esri-', '')
-        geojson = gm.geojsons.request_livingatlas_geojson(esri_location)
+        geojson = gm.data.get_livingatlas_geojson(esri_location)
         shp = os.path.join(user_workspace, '___esri.shp')
-        gm.geojsons.geojson_to_shp(geojson, shp)
-        timeseries = gm.netcdfs.shp_series(files, data['variable'], shp, date_pattern)
+        gm.convert.geojson_to_shapefile(geojson, shp)
+        timeseries = gm.times.shp_series(files, data['variable'], x_var='lon', y_var='lat')
         for file in glob.glob(os.path.join(user_workspace, '___esri.*')):
             os.remove(file)
         meta['seriesmsg'] = 'Within ' + esri_location
 
-    dates = timeseries.index.strftime('%Y-%m-%d')
+    dates = timeseries['times'].dt.strftime('%Y-%m-%d')
     dates = dates.tolist()
-    values = timeseries['values'].tolist()
+    values = list(map(float, timeseries['values'].tolist()))
 
     if data['stats']:
         return {
@@ -124,19 +119,22 @@ def makestatplots(df, time):
         numyears = 10
     years = [i + ref_yr for i in range(numyears)]
 
+    df.index = df['times']
+    del df['times']
+
     for i in range(1, 13):
         tmp = df[df.index.month == i]['values']
         ymin = min(tmp)
         ymax = max(tmp)
         mean = sum(tmp) / len(tmp)
-        monthbox.append((months[i], tmp.to_list()))
-        monthmulti.append((months[i], ymin, mean, ymax))
+        monthbox.append((months[i], list(map(float, tmp.to_list()))))
+        monthmulti.append((months[i], float(ymin), float(mean), float(ymax)))
     for year in years:
         tmp = df[df.index.year == year]['values']
         ymin = min(tmp)
         ymax = max(tmp)
         mean = sum(tmp) / len(tmp)
-        yearbox.append((year, tmp.to_list()))
-        yearmulti.append((year, ymin, mean, ymax))
+        yearbox.append((year, list(map(float, tmp.to_list()))))
+        yearmulti.append((year, float(ymin), float(mean), float(ymax)))
 
     return yearmulti, monthmulti, yearbox, monthbox
